4a1600cec494c3983cd4722a18aa2553e172b1f0
valiva056 <valivauppalapati@gmail.com>
2025-03-22 16:24:27 +0530

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,24 @@
+#write a function that two numbers as input and perform addition
+def add(a,b):
+     print(a+b)
+add(1,2)
+
+#create a function that accepts a name and displaya personalised greeting
+def greet(name):
+    print("hello"+name)
+greet("wordl")
+
+#write a function to calculate square of given number and show the result
+def square(c):
+     print(c*c)
+square(5) 
+
+#write a function to multiply two numberd and display the result
+def mul(e,f):
+    print(e*f)
+mul(3,20)
+
+#create a function that takes a string as input and prints the length of string
+def str_size(word):
+    print(len(word))
+str_size("happybirthday") 
