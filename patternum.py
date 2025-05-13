bb5a91efa9438fea92bc9f9c198ea02906f8424b
valiva056 <valivauppalapati@gmail.com>
2025-05-13 22:23:30 +0530

    Add files via upload

---
+++ patternum.py
@@ -0,0 +1,16 @@
+rows=5
+for i in range(rows,0,-1):
+    print(str(i)*i)
+    
+
+rows=5
+for i in range(rows,0,-1):
+    print("*"*i)
+    
+    
+rows=5
+for i in range(1,rows+1):
+    if i==3:
+        print("*"*5)
+    else:
+        print("*"*2)
